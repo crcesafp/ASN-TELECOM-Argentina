fefb5bd99992d58c575b21bc62bb4c1d3755ff3b
crcesafp <fpalacios@grupocesa.com>
2026-07-02 10:17:51 -0600

    Update ASN7303_TELECOM_AR.py

--- ASN7303_TELECOM_AR.py
+++ ASN7303_TELECOM_AR.py
@@ -1,1 +1,24 @@
+import requests
 
+ASNS = [
+    "AS7303"
+]
+
+prefixes = set()
+
+for asn in ASNS:
+
+    url = f"https://stat.ripe.net/data/announced-prefixes/data.json?resource={asn}"
+
+    r = requests.get(url)
+
+    data = r.json()
+
+    for prefix in data['data']['prefixes']:
+        prefixes.add(prefix['prefix'])
+
+with open("ASN7303-Ar.txt", "w") as f:
+    for p in sorted(prefixes):
+        f.write(p + "\n")
+
+print("Done.")
